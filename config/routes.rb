3c35cced500c131f0c1e61ac5bea0a1d1102ad74
alain <alainmanzi57@gmail.com>
2021-02-20 00:19:45 +0200

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  root 'static_pages#home'
+  get 'sessions/new'
+  resources:users
+  resources:sessions
+ root 'static_pages#home'
 get '/help',to: 'static_pages#help'
 get '/about',to: 'static_pages#about'
 get '/new',to: 'users#index'
+get'/login',to: 'sessions#new'
+post'/login',to: 'sessions#create'
+delete '/logout',to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
