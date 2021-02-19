c03b3608691d265b3d68d9fb9b845c70cd436857
alain <alainmanzi57@gmail.com>
2021-02-19 12:27:12 +0200

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   root 'static_pages#home'
 get '/help',to: 'static_pages#help'
 get '/about',to: 'static_pages#about'
+get '/new',to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
